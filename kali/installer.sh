8c0cb3fa03621d36628cab9bee7b8804fb298ec2
0Reab <122540000+0Reab@users.noreply.github.com>
2024-10-09 01:25:15 +0200

    Update installer.sh

--- kali/installer.sh
+++ kali/installer.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/bash
 
 # this script is meant to be run first for setting up your kali
+# Run as sudo otherwise it will throw ERR
 
+declare -a tools=(
+  "curl"
+  "tldr"
+  "locate"
+  "shellcheck"
+  "lolcat"
+  "neofetch"
+)
+
+for tool in ${tools[@]}; do 
+  yes | apt install "$tool" &>/dev/null
+  [ $? -eq 0 ] && echo "$tool - OK" || echo "$tool - ERR"
+done
+echo "base tools installed ..."
+sleep 3
 # setup custom tools
-
-toolbag="/custom_tools/"
+echo "setting up ..."
+DIR='/home/reab'
+toolbag="${PWD}/custom_tools/"
 
 cp "${toolbag}del.sh" /usr/local/bin/del
 cp "${toolbag}mkscript.sh" /usr/local/bin/mkscript
 
-# start installer for other tools
-
-sh ./tools.sh
+chmod +x /usr/local/bin/mkscript
+chmod +x /usr/local/bin/del
 
 # set php reverse shell as global var
 
-mkdir -p "~/scripts"
-cp "${toolbag}rev.php" "${HOME}/scripts/rev.php"
+mkdir -p "${DIR}/scripts"
+cp "${toolbag}rev.php" "${DIR}/scripts/rev.php"
 
 
 # setup .zshrc config file for terminal startup
@@ -24,15 +40,17 @@ cp "${toolbag}rev.php" "${HOME}/scripts/rev.php"
 function addline() {
         local line="$1"
 
-        echo "$line" >> "${HOME}/.bashrc"
+        echo "$line" >> "${DIR}/.zshrc"
 }
 
 # backup .bashrc file
-cp "${HOME}/.bashrc" "${HOME}/.bashrc_backup"
+cp "${DIR}/.bashrc" "${DIR}/.zshrc_backup"
 
 addline "# setup script lines"
 addline "alias mp='mousepad'"
 addline "alias roast='shellcheck'" # should install shell check before this runs
 addline "#neofetch | lolcat -F 0.03 -S 180 # Green" # install lolcat and neofetch
 addline "neofetch | lolcat -F 0.03 -S 60 # Purple"
-addline "export PHP='${HOME}/scripts/rev.php"
+addline "export PHP='${DIR}/scripts/rev.php'"
+
+source .zshrc &>/dev/null # use new config, errors are expected hence /dev/null redirect test it in new terminal window
