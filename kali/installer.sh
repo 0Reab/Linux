b420833888d9cc10bf2705c6b840573095116319
0Reab <122540000+0Reab@users.noreply.github.com>
2024-10-11 20:31:14 +0200

    Update installer.sh

--- kali/installer.sh
+++ kali/installer.sh
@@ -47,14 +47,15 @@ function addline() {
 # backup .bashrc file
 cp "${DIR}/.bashrc" "${DIR}/.zshrc_backup"
 
-addline "# setup script lines"
-addline "alias mp='mousepad'"
-addline "alias roast='shellcheck'" # should install shell check before this runs
-addline "#neofetch | lolcat -F 0.03 -S 180 # Green" # install lolcat and neofetch
-addline "neofetch | lolcat -F 0.03 -S 60 # Purple"
-addline "PHP='${DIR}/scripts/rev.php'"
-addline "PTY="   python -c 'import pty; pty.spawn(\"/bin/bash\")'\n   python3 -c 'import pty; pty.spawn(\"/bin/bash\")'""
-addline "alias shell='printf $PTY; nc -nvlp 4444'"
-
+addline "# CUSTOM setup script lines"
+addline "#neofetch | lolcat -F 0.03 -S 180 # Green" # cool terminal color variation
+addline "neofetch | lolcat -F 0.03 -S 60 # Purple" # cool terminal etc...
+addline "PHP='${DIR}/scripts/rev.php'" # path for classic php reverse shell
+addline "PTY="python -c 'import pty; pty.spawn(\"/bin/bash\")'\npython3 -c 'import pty; pty.spawn(\"/bin/bash\")'"" # shell upgrade commands
+addline "alias shell='printf $PTY; printf '\n'; nc -nvlp 4444'" # run netcat listener port 4444 display shell upgrade commands for python
+addline "alias copy='xclip -sel c <'" # copy contents of file to clipboard: <usage> copy file_name.txt
+addline "alias mp='mousepad'" # open mousepad txt editor: <usage> mp
+addline "alias roast='shellcheck'" # open shellcheck for bash script debugging <usgae> roast my_script.sh
 
 source .zshrc &>/dev/null # use new config, errors are expected hence /dev/null redirect test it in new terminal window
+# maby this last line is not needed if you write to .zshrc idk.
